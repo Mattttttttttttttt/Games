20fc869f2f3971c445aaa26add6d119df86e7be7
Матвей <matvei.ryabchitsky@yandex.ru>
2023-12-05 22:30:44 +0300

    Рабочая, но громоздкая К&Н

--- cross_zeros.py
+++ cross_zeros.py
@@ -9,8 +9,14 @@ df = pd.DataFrame(columns = col_names, index = [1, 2, 3])
 path = 'D:\Desktop\Программы Питон\Games\output'
 
 def user_move(position, user_id):
-    df.iloc[position[0]][position[1]] = 'X'
-    save(df, user_id)
+    if df.iloc[position[0]][position[1]] == '0':
+        return(df, 'Ошибка, это место уже занято')
+    elif df.iloc[position[0]][position[1]] == 'X':
+        return(df, 'Ошибка, вы уже ходили сюда')
+    else:
+        df.iloc[position[0]][position[1]] = 'X'
+        save(df, user_id)
+        return(df, 0)
 
 
 def save(df, user_id):
@@ -26,55 +32,70 @@ def save(df, user_id):
 
 def AI_move(df, user_id):
     lucky = 1
+    c_1 = 0
+    c_2 = 0
     position = [0, 0]
-    pos = [0, 0]
-    b = 0
     for i in range(3):
+        c_rows = 0
+        c_col = 0
         for j in range(3):
-            if df.iloc[i, j] == 'X':
-                pos = [i, j]
-                df.iloc[i, j] = 'x'
-                if 'X' in str(df.iloc[i, :]):
-                    df.iloc[pos[0], pos[1]] = 'X'
-                    for z in range(3):
-                        if df.iloc[i, z] != 'X' and df.iloc[i, z] != '0':
-                            df.iloc[i, z] = '0'
-                            save(df, user_id)
-                            b = 1
-                            lucky = 0
-                            break
-                elif 'X' in str(df.iloc[:, j]):
-                    df.iloc[pos[0], pos[1]] = 'X'
-                    for z in range(3):
-                        if df.iloc[z, j] != 'X' and df.iloc[z, j] != '0':
-                            df.iloc[z, j] = '0'
-                            save(df, user_id)
-                            b = 1
-                            lucky = 0
-                            break
-                df.iloc[i, j] = 'X'
-            if b == 1: break
-        if b == 1: break
-    for i in range(3):
-        if df.iloc[i, i] == 'X':
-            pos = [i, i]
-            df.iloc[i, i] = 'x'
+            if df.iloc[i][j] == 'X': c_col += 1
+            if df.iloc[j][i] == 'X': c_rows += 1
+            if c_col == 2:
+                for z in range(3):
+                    if df.iloc[i, z] != 'X' and df.iloc[i, z] != '0':
+                        df.iloc[i, z] = '0'
+                        save(df, user_id)
+                        return(df)
+            elif c_rows == 2:
+                for z in range(3):
+                    if df.iloc[z, i] != 'X' and df.iloc[z, i] != '0':
+                        df.iloc[z, i] = '0'
+                        save(df, user_id)
+                        return(df)
+        if df.iloc[i, i] == 'X': c_1 += 1
+        if df.iloc[i, 2 - i] == 'X': c_2 += 1
+        if c_1 == 2:
+            for z in range(3):
+                if df.iloc[z, z] != 'X' and df.iloc[z, z] != '0':
+                    df.iloc[z, z] = '0'
+                    save(df, user_id)
+                    return(df)
+        elif c_2 == 2:
+            for z in range(3):
+                if df.iloc[z, 2 - z] != 'X' and df.iloc[z, 2 - z] != '0':
+                    df.iloc[z, 2 - z] = '0'
+                    save(df, user_id)
+                    return(df)
 
     while lucky == 1:
-        print('Empty')
         position[0] = random.randint(0, 2)
         position[1] = random.randint(0, 2)
-        if pd.isnull(df.iloc[position[0], position[1]]) == True:
+        if pd.isnull(df.iloc[position[0], position[1]]):
             df.iloc[position[0], position[1]] = '0'
             save(df, user_id)
+            return (df)
             lucky = 0
 
 
-user_id = '1234'
-#save(df, user_id)
-df.iloc[0, 0] = 'X'
-df.iloc[1, 0] = '0'
-df.iloc[2, 0] = 'X'
-#df.iloc[0, 1] = 'X'
-AI_move(df, user_id)
-
+def check_win(df):
+    AI_win = [0, 0, 0, 0]
+    user_win = [0, 0, 0, 0]
+    nol = 0
+    for i in range(3):
+        for j in range(3):
+            if df.iloc[i, j] == 'X': user_win[0] += 1
+            if df.iloc[i, j] == '0': AI_win[0] += 1
+            if df.iloc[j, i] == 'X': user_win[1] += 1
+            if df.iloc[j, i] == '0': AI_win[1] += 1
+            if AI_win[0] == 3 or AI_win[1] == 3: return('Компьютер выиграл!')
+            if user_win[0] == 3 or user_win[1] == 3: return('Пользователь выиграл!')
+            if pd.isnull(df.iloc[i, j]): nol += 1
+        if df.iloc[i, i] == 'X': user_win[2] += 1
+        if df.iloc[i, i] == '0': AI_win[2] += 1
+        if df.iloc[i, 2 - i] == 'X': user_win[3] += 1
+        if df.iloc[i, 2 - i] == '0': AI_win[3] += 1
+        if AI_win[2] == 3 or AI_win[3] == 3: return ('Компьютер выиграл!')
+        if user_win[2] == 3 or user_win[3] == 3: return ('Пользователь выиграл!')
+    if nol == 0: return('Ничья! Место для ходов закончилось!')
+    return(1)
